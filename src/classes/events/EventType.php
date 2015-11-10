b6de083fda04436d96f3c795a22955e986cdbd5f
Edoardo Morassutto <gianluigi988@gmail.com>
2015-11-10 19:37:48 +0100

    Modificati gli id dei tipi di eventi

--- src/classes/events/EventType.php
+++ src/classes/events/EventType.php
@@ -3,9 +3,6 @@
 class EventType {
     const ClientConnected       = 1;
     const ClientDisconnected    = 2;
-    const FileUploaded          = 3;
-    const FileDownloaded        = 4;
-    const FileDeleted           = 5;
-    const ChannelCreated        = 6;
-    const ChannelDeleted        = 7;
+    const FileManager           = 3;
+    const Channel               = 4;
 }
