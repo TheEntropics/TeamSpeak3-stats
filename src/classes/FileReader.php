46f02a254f8efd8bc25d2300fb165e2518a8efa4
MarcoCivettini <civettini.marco97@gmail.com>
2015-11-11 08:54:44 +0100

    Implementata la classe FileReader

--- src/classes/FileReader.php
+++ src/classes/FileReader.php
@@ -2,7 +2,27 @@
 
 
 class FileReader {
+    private $logFiles;
+
+    public function __construct() {
+        $this->logFiles = array();
+        foreach(Config::LOG_FOLDERS as $i){
+            loadFolder($i);
+        }
+    }
+
     public function getLine() {
-        // TODO implementare questo metodo...
+
+    }
+
+    private function loadFolder($folder) {
+        $d = scandir($folder);
+        foreach ($d as $file) {
+            if ($file == "." || $file == "..") continue;
+            if (is_dir("$folder/$file"))
+                loadFolder("$folder/$file");
+            else
+                $this->logFiles[] = "$folder/$file";
+        }
     }
 }
