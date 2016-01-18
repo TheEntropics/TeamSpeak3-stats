240dc09b281d0ded193789a81dba35c07248509b
Edoardo Morassutto <gianluigi988@gmail.com>
2016-01-18 21:24:20 +0100

    Added bower

--- index.php
+++ index.php
@@ -9,11 +9,11 @@ Controller::init(true);
 <html>
 <head>
     <title>TeamSpeak3 - The Entropics</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="bower_components/bootstrap/dist/css/bootstrap-theme.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
-    <script src="js/jquery-2.1.4.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="js/utils.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
